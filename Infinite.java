5bb5d56796e2f3dd28ba34eb8287ebec28264399
14Kunalsharma <166681796+14Kunalsharma@users.noreply.github.com>
2025-09-07 08:03:23 +0530

    Add files via upload

---
+++ Infinite.java
@@ -0,0 +1,12 @@
+//Write an in infinite loop using do while condition.
+class Infinite{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any num: ");
+		int a = sc.nextInt();
+		do{
+			System.out.print("Current value of a: "+a);
+			a++;
+		}while(true);
+	}
+}
